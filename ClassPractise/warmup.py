f95eee09e3c946fbcfc05e2d795e3eac056afb6d
omar hussain <omarhussain23moh@gmail.com>
2023-09-25 12:30:19 -0300

    sum_dot_mat_vect

--- ClassPractise/warmup.py
+++ ClassPractise/warmup.py
@@ -37,19 +37,73 @@ matrix_B = [
 
 
 def isSquareMatrix(input) -> bool:
+
+    if (not isinstance(input, list)):
+        return False
+
     # length of 2D-Array gives number of rows
-    rows = len(input)
-    columns = 0
+    rows_len = len(input)
+    if rows_len == 0:
+        return False
+
     # count of length of item of array  gives number of columns
     for item in input:
-        columns = len(item)
-        break
-
-    if (rows == columns):
-        return True
-    else:
-        return False
+        columns_len = len(item)
+        if (not isinstance(item, list) or columns_len != rows_len):
+            return False
+    return True
 
 
 print(isSquareMatrix(matrix_A))
 print(isSquareMatrix(matrix_B))
+
+# Matrix Addition
+
+A = [
+    [1, 2, 3],
+    [2, 3, 4],
+    [5, 6, 7]
+]
+B = [
+    [4, 5, 6],
+    [7, 8, 9],
+    [-1, 9, 0]
+]
+
+
+def sum_matrix(A, B):
+    result = []
+
+    for i in range(len(A)):
+        sum_list = []
+        for j in range(len(B)):
+            print(A[i][j], B[i][j])
+            sum = A[i][j] + B[i][j]
+            sum_list.append(sum)
+        result.append(sum_list)
+
+    return result
+
+
+print(sum_matrix(A, B))
+
+# dot product of 2 matrices
+X = [1, 2, 3]
+Y = [4, 5, 6]
+
+def dot_product_vectors(X, Y):
+
+    if (len(X) != len(Y)):
+        print("Cannot dot product for unequal sized vectors")
+        exit()
+
+    
+    result = 0
+    for i in range(len(X)):
+        product = X[i] * Y[i]
+        result += product
+
+    return (result)
+
+
+print(dot_product_vectors(X, Y))
